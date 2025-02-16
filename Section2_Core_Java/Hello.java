443cf39ccb737dfbb3f271db74c098ea7c88d023
retrovibe51 <w.barretto.code@gmail.com>
2025-02-16 11:28:51 +0530

    started with classes and objects

--- Section2_Core_Java/Hello.java
+++ Section2_Core_Java/Hello.java
@@ -1,7 +1,16 @@
+class Calculator{
+    public int add(){
+        System.out.println("In add");
+        return 0;
+    }
+}
 
 class Hello{
     public static void main(String args[]){
-        
+        int num1=4;
+        int num2=5;
+        int result=num1+num2;
+        System.out.println(result);    
     }
 }
 
