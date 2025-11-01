6aff5ea50d39e6b69c96dce833d160ea78a0ad13
retrovibe51 <w.barretto.code@gmail.com>
2025-11-01 19:14:36 +0530

    reached hibernate saving data concept

--- Section6_Hibernate/hibproj/src/main/java/com/example/App.java
+++ Section6_Hibernate/hibproj/src/main/java/com/example/App.java
@@ -1,5 +1,14 @@
 package com.example;
 
+import java.lang.module.Configuration;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 /**
  * Hello world!
  *
@@ -8,6 +17,31 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Student s1 = new Student();
+        s1.setsName("Navin");
+        s1.setRollno(101);
+        s1.setsAge(30);
+
+        // Create registry
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+            .configure("hibernate.cfg.xml") // by default looks for hibernate.cfg.xml in classpath
+            .build();
+
+        // Create MetadataSources
+        MetadataSources sources = new MetadataSources(registry);
+
+        // Create Metadata
+        Metadata metadata = sources.getMetadataBuilder().build();
+        
+        SessionFactory sf = metadata.getSessionFactoryBuilder().build();
+
+        // Configuration cfg = new Configuration();
+
+        //From hibernate
+        Session session = sf.openSession();
+
+        session.save(s1);
+
+        System.out.println(s1);
     }
 }
--- Section6_Hibernate/hibproj/src/main/java/com/example/Student.java
+++ Section6_Hibernate/hibproj/src/main/java/com/example/Student.java
@@ -4,4 +4,26 @@ public class Student {
     private int rollno;
     private String sName;
     private int sAge;
+    public int getRollno() {
+        return rollno;
+    }
+    @Override
+    public String toString() {
+        return "Student [rollno=" + rollno + ", sName=" + sName + ", sAge=" + sAge + "]";
+    }
+    public void setRollno(int rollno) {
+        this.rollno = rollno;
+    }
+    public String getsName() {
+        return sName;
+    }
+    public void setsName(String sName) {
+        this.sName = sName;
+    }
+    public int getsAge() {
+        return sAge;
+    }
+    public void setsAge(int sAge) {
+        this.sAge = sAge;
+    }
 }
