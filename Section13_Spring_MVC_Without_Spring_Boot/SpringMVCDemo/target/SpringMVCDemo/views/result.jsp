0e0a729e939bada33af5329810df1c1e9633c37e
retrovibe51 <w.barretto.code@gmail.com>
2025-11-29 13:48:10 +0530

    done with Section 13 - Spring MVC without Spring Boot

---
+++ Section13_Spring_MVC_Without_Spring_Boot/SpringMVCDemo/target/SpringMVCDemo/views/result.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html; charset=UTF-8" %>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <h2>Welcome to Telecom Industry</h2>
+        <p>${alien1}</p>  
+        <p>Welcome to the ${course} World</p>
+    </body>
+</html>
---
+++ Section13_Spring_MVC_Without_Spring_Boot/spring_mvc_demo/src/main/java/com/telecom/spring_mvc_demo/HomeController.java
@@ -0,0 +1,76 @@
+package com.telecom.spring_mvc_demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class HomeController {
+
+    @ModelAttribute("course")
+    public String courseName(){
+        return "Java";
+    }
+    
+    @RequestMapping("/")
+    public String home(){
+        System.out.println("Home method called");
+        return "index";
+    }
+
+    @RequestMapping("addAlien")
+    //int num1, int num2 ar e query parameters
+    public String addAlien(@ModelAttribute("alien1") Alien alien){
+        return "result";
+    }
+
+    @RequestMapping("addAlien2")
+    //int num1, int num2 ar e query parameters
+    public ModelAndView addAlien2(int aid, String aname, ModelAndView mv){
+        Alien alien = new Alien();
+        alien.setAid(aid);
+        alien.setAname(aname);
+
+        mv.addObject("alien", alien);
+        mv.setViewName("result");
+        return mv;
+    }
+
+    @RequestMapping("add")
+    //int num1, int num2 ar e query parameters
+    public ModelAndView add(@RequestParam("num1") int num, int num2, ModelAndView mv){
+        int result = num+num2;
+        
+        mv.addObject("result", result);
+        mv.setViewName("result");
+
+        return mv;
+    }
+
+    @RequestMapping("add-alternate-2")
+    //int num1, int num2 ar e query parameters
+    public String add_alternate_2(@RequestParam("num1") int num, int num2, Model model){
+        int result = num+num2;
+        
+        model.addAttribute("num1", num);
+        model.addAttribute("num2", num2);
+        model.addAttribute("result",result);
+        return "result";
+    }
+    
+    // @RequestMapping("add-alternate")
+    // public String addAlternate(HttpServletRequest req, HttpSession session){
+    //     int num1 = Integer.parseInt(req.getParameter("num1"));
+    //     int num2 = Integer.parseInt(req.getParameter("num2"));
+    //     int result = num1+num2;
+        
+    //     session.setAttribute("num1", num1);
+    //     session.setAttribute("num2", num2);
+    //     session.setAttribute("result",result);
+    //     return "result.jsp";
+    // }
+}
