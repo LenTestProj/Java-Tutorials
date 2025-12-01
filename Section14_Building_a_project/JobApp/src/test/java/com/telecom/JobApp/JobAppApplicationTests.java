c4c4f355da0a98520b7ea5d9bd86b1721cbd9472
LenTestProj <lennfernz2@gmail.com>
2025-12-01 10:14:15 +0530

    started with section 14 - building a project

---
+++ Section14_Building_a_project/JobApp/src/test/java/com/telecom/JobApp/JobAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telecom.JobApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
