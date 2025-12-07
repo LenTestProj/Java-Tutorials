f3b03bb7e5cfa1dd0ac9000612bc268d380e26ef
LenTestProj <lennfernz2@gmail.com>
2025-12-07 22:36:07 +0530

    started with section 17 - Spring Boot MVC

---
+++ Section17_Spring_Boot_MVC/SpringEcom/src/main/java/com/telecom/SpringEcom/repo/ProductRepo.java
@@ -0,0 +1,24 @@
+package com.telecom.SpringEcom.repo;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.telecom.SpringEcom.model.Product;
+
+@Repository
+public interface ProductRepo extends JpaRepository<Product, Integer>{
+
+    //Here we use JPA specific sql we use model name not table name
+    @Query("SELECT p from Product p WHERE "+
+        "LOWER(p.name) LIKE LOWER(CONCAT('%',:keyword,'%')) OR "+
+        "LOWER(p.description) LIKE LOWER(CONCAT('%',:keyword,'%')) OR "+
+        "LOWER(p.brand) LIKE LOWER(CONCAT('%',:keyword,'%')) OR "+
+        "LOWER(p.category) LIKE LOWER(CONCAT('%',:keyword,'%'))"
+    )
+    List<Product> searchProducts(@Param("keyword") String keyword);
+    
+}
