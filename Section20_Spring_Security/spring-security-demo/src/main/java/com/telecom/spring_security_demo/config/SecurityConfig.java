1768bc834a3dd7aacf67c973eb56f5c860252a0a
LenTestProj <lennfernz2@gmail.com>
2025-12-20 12:11:51 +0530

    done with section 20 - spring security

--- Section20_Spring_Security/spring-security-demo/src/main/java/com/telecom/spring_security_demo/config/SecurityConfig.java
+++ Section20_Spring_Security/spring-security-demo/src/main/java/com/telecom/spring_security_demo/config/SecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -29,7 +30,8 @@ public class SecurityConfig {
     @Bean
     public AuthenticationProvider authProvider(){
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
-        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        // provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
         return provider;
     }
     
