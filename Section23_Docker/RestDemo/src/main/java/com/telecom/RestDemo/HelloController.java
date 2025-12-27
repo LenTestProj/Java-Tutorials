693c38b972919e922eaf01d4c94b961ca1b56d2f
LenTestProj <lennfernz2@gmail.com>
2025-12-27 15:32:15 +0530

    started with Section 23 - Docker

---
+++ Section23_Docker/RestDemo/src/main/java/com/telecom/RestDemo/HelloController.java
@@ -0,0 +1,12 @@
+package com.telecom.RestDemo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/")
+    public String greet(){
+        return "Hello World";
+    }
+}
