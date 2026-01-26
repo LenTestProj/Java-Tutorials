07eaf201da007865ac82f1c6b1aee5a65a30718d
LenTestProj <lennfernz2@gmail.com>
2026-01-26 17:36:56 +0530

    started with Section 26 - Microservices

---
+++ Section26_MIcroservices/Monolithic/quizApp/src/main/java/com/telecom/quizApp/service/QuizService.java
@@ -0,0 +1,33 @@
+package com.telecom.quizApp.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.telecom.quizApp.dao.QuestionDao;
+import com.telecom.quizApp.dao.QuizDao;
+import com.telecom.quizApp.model.Question;
+import com.telecom.quizApp.model.Quiz;
+
+@Service
+public class QuizService {
+    @Autowired
+    QuizDao quizDao;
+
+    @Autowired
+    QuestionDao questionDao;
+
+    public ResponseEntity<String> createQuiz(String category, int numQ, String title) {
+
+        List<Question> questions = questionDao.findRandomQuestionsByCategory(category,numQ);
+
+        Quiz quiz = new Quiz();
+        quiz.setTitle(title);
+        quiz.setQuestions(questions);
+        quizDao.save(quiz);
+        return new ResponseEntity<>("Success", HttpStatus.CREATED);
+    }
+}
